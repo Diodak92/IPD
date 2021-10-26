73e0b0b0ddc3371235ff9d0b1ea971bb5ab45b35
Diodak92 <49007170+Diodak92@users.noreply.github.com>
2021-10-27 02:11:14 +0300

    Update remote_circuit_braker_test.py
    Added console countdown timer

--- remote_circuit_braker_test.py
+++ remote_circuit_braker_test.py
@@ -34,18 +34,27 @@ def set_SSR(state):
     resp = client.write_register(4211, state, unit=SM4_MODBUS_ADDR)
     return(resp)
 
-def set_MDM(state):
-    resp = client.write_register(4212, state, unit=SM4_MODBUS_ADDR)
-    return(resp)
+def  countdown(seconds):
+    print("CIRCUIT BRAKER OVERLOADED!!! RESET IN: ")
+    for i in range (seconds, 0, -1):
+        sleep(1)
+        print(i, " s")
+
+def reset_MDM(seconds):
+    countdown(seconds)
+    resp1 = client.write_register(4212, 0x01, unit=SM4_MODBUS_ADDR)
+    sleep(1)
+    resp2 = client.write_register(4212, 0x00, unit=SM4_MODBUS_ADDR)
+    return(resp1, resp2)
 
 ### MAIN PROGRAM ###
 cycles = int(input("Enter number of overload cycles: "))
-wait_time = float(input("Enter time to reset overloaded circuit braker [s]: "))
+wait_time = int(input("Enter time to reset overloaded circuit braker [s]: "))
 
 on = 1
 off = 0
-
 i = 1
+
 print("STARTING AUTOMATIC CIRCUIT BRAKER OVERLOAD TEST\n")
 print("TEST: ", i)
 while(cycles>=i):
@@ -54,10 +63,7 @@ while(cycles>=i):
     cb_state = read_casd_state()
     if(cb_state[2] == 1):#(cb_state[0] == 1 or cb_state[2] == 1):
         print("CB-open: ", cb_state[0]," CB-closed: ", cb_state[1], " CB-tripped: ", cb_state[2])
-        sleep(wait_time)
-        set_MDM(on)
-        sleep(1)
-        set_MDM(off)
+        reset_MDM(wait_time)
         print("\n")
         i = i+1
         print("TEST: ", i)
