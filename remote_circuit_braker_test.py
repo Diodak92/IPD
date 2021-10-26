7dbea04effbcfaa3aae07b77578cc99af6ef3562
Diodak92 <49007170+Diodak92@users.noreply.github.com>
2021-10-26 23:45:09 +0200

    Update remote_circuit_braker_test.py
    First tested and working version

--- remote_circuit_braker_test.py
+++ remote_circuit_braker_test.py
@@ -1,4 +1,3 @@
-
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 from time import sleep 
 import logging
@@ -11,6 +10,7 @@ log = logging.getLogger()
 log.setLevel(logging.ERROR)
 
 # initialize a serial RTU client instance
+# CHECK CORRECT COM PORT! (On Windows in Device Mennager) 
 client = ModbusClient(method="rtu", port="COM8", stopbits=1, bytesize=8, parity='N', baudrate=38400)
 # Connect to the serial modbus server
 connection = client.connect()
@@ -18,13 +18,9 @@ print("got connection : ", connection)
 
 ### FUNCTIONS DEFINITION ###
 def read_casd_state():
-    result = client.read_discrete_inputs(0x4000, 3, uint=EMSCX3_MODBUS_ADDR)
-    if result: 
-        status1 = result.bits[0]
-        status2 = result.bits[1]
-        status3 = result.bits[2]
-        print("CB-open: ",status1, "CB-closed: ", status2, " CB-tripped: ", status3)
-        return([status1, status2, status3])
+    result = client.read_discrete_inputs(0x4000, 3, unit=10)
+    #print(result)
+    return(result.bits[0:3])    
 
 def read_sm4_state():
     result = client.read_holding_registers(4211, 2, unit=SM4_MODBUS_ADDR)
@@ -44,29 +40,27 @@ def set_MDM(state):
 
 ### MAIN PROGRAM ###
 cycles = int(input("Enter number of overload cycles: "))
-wait_time = float(input("Enter test loop cycle time min 30 [s]: "))
-if wait_time <= 30:
-    wait_time = 30
+wait_time = float(input("Enter time to reset overloaded circuit braker [s]: "))
 
-read_sm4_state()
-read_casd_state()
-sec = 1
 on = 1
 off = 0
 
-for i in range(1, cycles+1):
-    print("TEST: ", i)
+i = 1
+print("STARTING AUTOMATIC CIRCUIT BRAKER OVERLOAD TEST\n")
+print("TEST: ", i)
+while(cycles>=i):
     set_SSR(on)
-    sleep(sec)
-    set_SSR(off)
+    sleep(1)
     cb_state = read_casd_state()
-    if (cb_state[0] == 1 or cb_state[2] == 1):
-        sleep(sec)
+    if(cb_state[2] == 1):#(cb_state[0] == 1 or cb_state[2] == 1):
+        print("CB-open: ", cb_state[0]," CB-closed: ", cb_state[1], " CB-tripped: ", cb_state[2])
+        sleep(wait_time)
         set_MDM(on)
-        sleep(sec)
+        sleep(1)
         set_MDM(off)
         print("\n")
-        sleep(wait_time-3*sleep)
-
+        i = i+1
+        print("TEST: ", i)
+set_SSR(off)
 print("Test done, outlet disconnected")
 client.close()
